ced2feaf1348492d61aa2793a9e1246ad9990351
Naomi T. S <tsnaomi@uw.edu>
2017-01-04 20:25:43 -0800

    clean up linked list tests

--- new/test.py
+++ new/test.py
@@ -7,76 +7,92 @@ from linked_list import LinkedList
 class TestLinkedList(unittest.TestCase):
 
     def setUp(self):
-        self.List = LinkedList()
         self.emptyList = LinkedList()
+        self.List = LinkedList()
 
-    def test_insert(self):
-        #
-        for i in range(1, 5):
-            self.List.insert(i)
-
-        self.assertEqual(repr(self.List), '[4, 3, 2, 1]')
-        self.assertEqual(repr(self.emptyList), '[]')
-
-    def test_append(self):
-        #
+        # populate list with 1, 2, 3, 4
         for i in range(1, 5):
             self.List.append(i)
-        self.emptyList.append(8)
 
-        self.assertEqual(repr(self.List), '[1, 2, 3, 4]')
-        self.assertEqual(repr(self.emptyList), '[8]')
+    def test_tup(self):
+        '''Test returning the linked list as a tuple literal.'''
+        self.assertEqual(self.emptyList.tup(), ())
+        tup = (1, 3.14, 'foo', True)
+
+        for i in tup:
+            self.emptyList.append(i)
+
+        self.assertEqual(self.emptyList.tup(), tup)
+
+    def test_repr(self):
+        '''Test returning the linked list as a string.'''
+        self.assertEqual(repr(self.emptyList), '()')
+        tup = (1, 3.14, 'foo', True)
+
+        for i in tup:
+            self.emptyList.append(i)
+
+        self.assertEqual(repr(self.emptyList), "(1, 3.14, 'foo', True)")
+
+    def test_insert(self):
+        '''Test inserting values at the head of list the list.'''
+        for i in range(1, 5):
+            self.emptyList.insert(i)
+
+        self.assertEqual(self.emptyList.tup(), (4, 3, 2, 1))
+
+    def test_append(self):
+        '''Test appending values at the end of the list.'''
+        self.List.append(True)
+        self.assertEqual(self.List.tup(), (1, 2, 3, 4, True))
 
     def test_pop(self):
-        #
-        for i in range(1, 5):
-            self.List.insert(i)
-        self.assertEqual(self.List.pop(), 4)
-        self.assertEqual(repr(self.List), '[3, 2, 1]')
-        self.List.pop()
-        self.List.pop()
+        '''Test popping off and returning the head of the list.'''
         self.assertEqual(self.List.pop(), 1)
-        self.assertEqual(repr(self.List), '[]')
+        self.assertEqual(self.List.tup(), (2, 3, 4))
+        self.assertEqual(self.List.pop(), 2)
+        self.assertEqual(self.List.pop(), 3)
+        self.assertEqual(self.List.pop(), 4)
+        self.assertEqual(self.List.tup(), ())
+
         with self.assertRaises(IndexError):
             self.emptyList.pop()
 
     def test_shift(self):
-        #
-        for i in range(1, 3):
-            self.List.insert(i)
-        self.assertEqual(self.List.shift(), 1)
-        self.assertEqual(repr(self.List), '[2]')
+        '''Test removing and return the tail of the list.'''
+        self.assertEqual(self.List.shift(), 4)
+        self.assertEqual(self.List.tup(), (1, 2, 3))
+        self.assertEqual(self.List.shift(), 3)
         self.assertEqual(self.List.shift(), 2)
-        self.assertEqual(repr(self.List), '[]')
+        self.assertEqual(self.List.shift(), 1)
+        self.assertEqual(self.List.tup(), ())
+
         with self.assertRaises(IndexError):
             self.emptyList.shift()
 
     def test_remove(self):
-        #
-        for i in range(2):
-            for j in range(1, 5):
-                self.List.append(j)
+        '''Testing finding and removing values from the list.'''
+        for i in range(1, 5):
+            self.List.append(i)
+
         self.List.remove(2)
-        self.assertEqual(repr(self.List), '[1, 3, 4, 1, 2, 3, 4]')
+        self.assertEqual(self.List.tup(), (1, 3, 4, 1, 2, 3, 4))
+
         with self.assertRaises(ValueError):
             self.List.remove(6)
 
     def test_size(self):
-        for i in range(10):
-            self.List.insert(i)
-        self.assertEqual(self.List.size(), 10)
+        '''Test returning the length of the list.'''
+        for i in range(100):
+            self.emptyList.insert(i)
+
+        self.assertEqual(self.emptyList.size(), 100)
 
     def test_contains(self):
-        for i in range(4):
-            self.List.insert(i)
+        '''Test returning T/F for whether the list contains certain values.'''
         self.assertTrue(self.List.contains(2))
-        self.assertFalse(self.List.contains(4))
+        self.assertFalse(self.List.contains(10))
 
-    def test_repr(self):
-        tup = (1, 3.14, 'foo', True)
-        for i in tup:
-            self.List.append(i)
-        self.assertEqual(self.List._print(), tup)
 
 if __name__ == '__main__':
     unittest.main()
