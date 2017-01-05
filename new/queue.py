db91cc9f5c8f8a50b2eb8b0aac007224e3122247
Naomi T. S <tsnaomi@uw.edu>
2017-01-04 22:39:36 -0800

    clean up Queue.py

--- new/queue.py
+++ new/queue.py
@@ -1,11 +1,5 @@
 # coding='utf-8'
 
-#   - enqueue(value): adds value to the queue
-#   - dequeue(): removes the correct item from the queue and returns its value
-#     (should raise an error if the queue is empty)
-#   - size(): return the size of the queue.  Should return 0 if the queue is
-#     empty.
-
 
 class Node:
 
