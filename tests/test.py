369860db5b8a392155354cf95488d93a7314cb11
Naomi T. S <tsnaomi@uw.edu>
2017-01-07 16:24:08 -0800

    consolidate old tests into tests/test.py

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,173 @@
-#!usr/bin/env Python
+# !/usr/bin/env Python
 
 import unittest
 
-from bst import Tree
+from algorithms import insert_sort, merge_sort
+from data_structures import HashTable, LinkedList, Queue, Stack, BSTree
+from nltk.corpus import gutenberg  # any excuse to use NLTK
 
 
-class TestTree(unittest.TestCase):
+# Data structure tests --------------------------------------------------------
+
+# test singly-linked list
+class TestLinkedList(unittest.TestCase):
+
+    def setUp(self):
+        self.foo = LinkedList()
+        self.list1 = '(4, 3, 2, 1)'
+        self.list2 = '(3, 2, 1)'
+        self.list3 = '(4, 2, 1, 0)'
+        self.list4 = '(bacon, 3.14, True, 4, 3, 2, 1)'
+
+    def test_insert(self):
+        for i in range(1, 5):
+            self.foo.insert(i)
+        self.assertEqual(self.foo.__str__(), self.list1)
+
+    def test_pop(self):
+        for i in range(1, 5):
+            self.foo.insert(i)
+        self.assertEqual(self.foo.pop(), 4)
+        self.assertEqual(self.foo.__str__(), self.list2)
+
+    def test_size(self):
+        for i in range(10):
+            self.foo.insert(i)
+        self.assertEqual(self.foo.size(), 10)
+
+    def test_search(self):
+        for i in range(4):
+            self.foo.insert(i)
+        self.assertEqual(self.foo.search(2).value, 2)
+
+    def test_remove(self):
+        for i in range(5):
+            self.foo.insert(i)
+        self.foo.remove(3)
+        self.assertEqual(self.foo.__str__(), self.list3)
+
+    def test_PRINT(self):
+        for i in range(1, 5):
+            self.foo.insert(i)
+        self.foo.insert(True)
+        self.foo.insert(3.14)
+        self.foo.insert('bacon')
+        self.assertEqual(self.foo.PRINT(), self.list4)
+
+
+class TestStack(unittest.TestCase):
+
+    def setUp(self):
+        self.breakfast = Stack()
+        self.meal1 = ['pineapple juice', 'sausage', 'eggs', 'PANCAKES']
+        self.meal2 = [2, 1, 0]
+
+    def amass(self, node):
+        nodes = []
+        while node:
+            nodes.append(node.value)
+            node = node.next
+        return nodes
+
+    def test_push(self):
+        self.breakfast.push('PANCAKES')
+        self.breakfast.push('eggs')
+        self.breakfast.push('sausage')
+        self.breakfast.push('pineapple juice')
+        nodes = self.amass(self.breakfast.node)
+        self.assertEqual(nodes, self.meal1)
+
+    def test_pop(self):
+        for i in range(4):
+            self.breakfast.push(i)
+        returned = self.breakfast.pop()
+        nodes = self.amass(self.breakfast.node)
+        self.assertEqual(returned, 3)
+        self.assertEqual(nodes, self.meal2)
+
+
+class TestQueue(unittest.TestCase):
+
+    def setUp(self):
+        self.queue = Queue()
+        self.list1 = [4, 3, 2, 1]
+        self.list2 = [3, 2, 1]
+
+    def amass(self, node):
+        nodes = []
+        while node:
+            nodes.insert(0, node.value)
+            node = node.next
+        return nodes
+
+    def test_enqueue(self):
+        for i in range(1, 5):
+            self.queue.enqueue(i)
+        nodes = self.amass(self.queue.head)
+        self.assertEqual(nodes, self.list1)
+
+    def test_dequeue(self):
+        for i in range(4):
+            self.queue.enqueue(i)
+        returned = self.queue.dequeue()
+        nodes = self.amass(self.queue.head)
+        self.assertEqual(returned, 0)
+        self.assertEqual(nodes, self.list2)
+
+    def test_size(self):
+        for i in range(10):
+            self.queue.enqueue(i)
+        self.assertEqual(self.queue.size(), 10)
+
+
+class TestHashTable(unittest.TestCase):
+
+    def setUp(self):
+        self.TABLE = HashTable()
+        self.ALICE = list(set(gutenberg.words('carroll-alice.txt')))[:400]
+
+    def test_hash(self):
+        self.assertEqual(self.TABLE.hash('Alice in Wonderland, circa 1865'), 5)
+        with self.assertRaises(TypeError):
+            self.TABLE.hash(1234)
+
+        # HashTable accurately handles whatever number of containers specified
+        TABLE2 = HashTable(100)
+        self.assertEqual(TABLE2.hash('Alice in Wonderland, circa 1865'), 29)
+
+    def test_set(self):
+        for word in self.ALICE:
+            self.TABLE.set(word, word)
+            bucket = self.TABLE.head
+            for num in range(self.TABLE.hash(word)):
+                bucket = bucket.next
+            self.assertIsNotNone(bucket.value.search((word, word)))
+        with self.assertRaises(TypeError):
+            self.TABLE.set(4, 4)
+
+    def test_get(self):
+        for word in self.ALICE:
+            self.TABLE.set(word, word)
+            value = self.TABLE.get(word)
+            self.assertEqual(value, word)
+        with self.assertRaises(TypeError):
+            self.TABLE.get(4)
+        with self.assertRaises(KeyError):
+            self.TABLE.get('Curiouser and curiouser!')
+
+    def test_resetting_and_getting_new_values(self):
+        for word in self.ALICE:
+            self.TABLE.set(word, word)
+        container = self.TABLE.head
+        for num in range(self.TABLE.hash('Alice')):
+            container = container.next
+        self.assertIn(str(('ALICE', 'ALICE')), str(container.value))
+        self.TABLE.set('ALICE', 'Down the Rabit-Hole')
+        self.assertEqual(self.TABLE.get('ALICE'), 'Down the Rabit-Hole')
+        self.assertNotIn(str(('ALICE', 'ALICE')), str(container.value))
+
+
+class TestBinarySearchTree(unittest.TestCase):
 
     def setUp(self):
         self.tree = Tree(4)
@@ -25,7 +187,7 @@ class TestTree(unittest.TestCase):
 
         # init with a forbidden value
         with self.assertRaises(TypeError):
-            TREE = Tree('leaves')
+            Tree('leaves')
 
     def test_insert(self):
         # insert into a non-empty tree
@@ -223,6 +385,64 @@ class TestTree(unittest.TestCase):
         for i in self.TREE.breadth_first():
             test.append(i)
         self.assertEqual(test, expected)
+
+
+# Algorithm tests -------------------------------------------------------------
+
+class TestInsertionSort(unittest.TestCase):
+
+    def setUp(self):
+        self.list = [3, 1, 4, 5, 9, 2, 6]
+        self.sorted = [1, 2, 3, 4, 5, 6, 9]
+
+    def test_insertSort(self):
+        self.assertEqual(insert_sort(self.list), self.sorted)
+
+    def test_insertSort_empty(self):
+        self.assertEqual(insert_sort([]), [])
+
+    def test_insertSort_single(self):
+        self.assertEqual(insert_sort([3]), [3])
+
+    def test_insertSort_duplicates(self):
+        self.list.append(4)
+        expected = [1, 2, 3, 4, 4, 5, 6, 9]
+        self.assertEqual(insert_sort(self.list), expected)
+
+    def test_insertSort_sorted(self):
+        self.assertEqual(insert_sort(sorted(self.list)), self.sorted)
+
+    def test_insertSort_reversed(self):
+        self.assertEqual(insert_sort(sorted(self.list)[::-1]), self.sorted)
+
+
+class TestMergeSort(unittest.TestCase):
+
+    def setUp(self):
+        self.list = [3, 1, 4, 5, 9, 2, 6]
+        self.sorted = [1, 2, 3, 4, 5, 6, 9]
+
+    def test_insertSort(self):
+        self.assertEqual(merge_sort(self.list), self.sorted)
+
+    def test_insertSort_empty(self):
+        self.assertEqual(merge_sort([]), [])
+
+    def test_insertSort_single(self):
+        self.assertEqual(merge_sort([3]), [3])
+
+    def test_insertSort_duplicates(self):
+        self.list.append(4)
+        expected = [1, 2, 3, 4, 4, 5, 6, 9]
+        self.assertEqual(merge_sort(self.list), expected)
+
+    def test_insertSort_sorted(self):
+        self.assertEqual(merge_sort(sorted(self.list)), self.sorted)
+
+    def test_insertSort_reversed(self):
+        self.assertEqual(merge_sort(sorted(self.list)[::-1]), self.sorted)
+
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     unittest.main()
